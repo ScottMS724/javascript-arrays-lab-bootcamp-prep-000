6736ee0a0cb62587a6a1bc993a8adbf260349455
Scott Silverman <scottms724@gmail.com>
2018-01-23 16:02:27 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -1,1 +1,30 @@
-const app = "I don't do much."
+function destructivelyAppendKitten(name) {
+  kittens.push(name)
+  return kittens
+}
+
+function destructivelyPrependKitten(name) {
+  kittens.unshift(name)
+  return kittens
+}
+
+function destructivelyRemoveLastKitten() {
+  kittens.pop()
+}
+
+function destructivelyRemoveFirstKitten() {
+  kittens.shift()
+}
+
+function appendKitten(name) {
+  return kittens.concat(name)
+}
+
+function prependKitten(name) {
+  var morekittens = [name, ...kittens]
+  return morekittens
+}
+
+function removeLastKitten() {
+  kittens.slice(-1)
+}
